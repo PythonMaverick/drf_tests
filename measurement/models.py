53eb06e91a5719bb0444d3ba94b863ac4de6d33e
Marat Movlamov <marat.movlamov@moonbug.com>
2022-11-08 23:29:11 +0300

    Sceleton Added

---
+++ measurement/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+
+# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
---
+++ measurement/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView

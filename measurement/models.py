9d1abeb8ce5f252154c34e2062686569685ccde8
Marat Movlamov <marat.movlamov@moonbug.com>
2022-11-08 23:41:45 +0300

    Sensor and Measurement Models are added to the models layer

--- measurement/models.py
+++ measurement/models.py
@@ -2,5 +2,13 @@ from django.db import models
 
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+class Sensor(models.Model):
+    name = models.TextField(max_length=25)
+    description = models.TextField(max_length=100)
 
 
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    measured_at = models.DateField(auto_now=True)
+    image = models.ImageField(null=True, upload_to='media')
